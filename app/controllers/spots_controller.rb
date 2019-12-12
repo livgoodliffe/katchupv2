e523d7b0a42b25e9af32d630edaa5c030f13aaa8
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-12-12 22:59:12 +1100

    search should work

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -7,20 +7,18 @@ class SpotsController < ApplicationController
 
   def index
     term = params[:query]
+    @client = Romato::Zomato.new(ENV["ZOMATO_API"])
+    @results = @client.get_search({ q: term, lat: current_user.latitude, lon: current_user.longitude })
 
     @spots_db = Spot.where('name ILIKE ?', "%#{term}%")
+    @zom_results = @results["restaurants"]
+    @spots_zom = []
 
-    @client = Romato::Zomato.new(ENV["ZOMATO_API"])
-
-    @results = @client.get_search({ q: term, lat: current_user.latitude, lon: current_user.longitude })
-    # @zom_results = @results["restaurants"]
-    # @spots_zom = []
-
-    # @zom_results.each do |spot|
-    #   zom_res_id = spot["restaurant"]["R"]["res_id"]
-    #   result = Spot.exists?(res_id: zom_res_id)
-    #   @spots_zom.push(spot) if result == false
-    # end
+    @zom_results.each do |spot|
+      zom_res_id = spot["restaurant"]["R"]["res_id"]
+      result = Spot.exists?(res_id: zom_res_id)
+      @spots_zom.push(spot) if result == false
+    end
 
     if params[:query]
       respond_to do |format|
