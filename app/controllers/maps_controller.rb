f75c23fcd07d528b96132e3efca5a8fa3cc4f39e
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-12-14 10:52:32 +1100

    map multiple marker types

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,37 +1,30 @@
 class MapsController < ApplicationController
   def index
-    @spots = Spot.all
+    wishlist_spots = current_user.wishlist_spots
+    visited_spots = current_user.visited_spots
 
-    @markers = @spots.map do |spot|
+    @markers_wishlist = wishlist_spots.map do |spot|
       {
         lat: spot.latitude,
         lng: spot.longitude,
         infoWindow: render_to_string(partial: "maps/info-window", locals: { spot: spot }),
-        image_url: helpers.asset_url('logo.png')
+        image_url: helpers.asset_url('wishlist.png')
       }
     end
 
-    # @marker_user = create_user_marker if user_has_coords?
-    # @marker_user_avatar = current_user.thumbnail
-
-    # wishlist_spots = current_user.wishlist_spots
-    # @markers_wishlist = create_markers(wishlist_spots)
-
-    # visited_spots = current_user.visited_spots
-    # @markers_visited = create_markers(visited_spots)
-  end
-
-  private
-
-  def create_markers(spots)
-    spots.map do |spot|
+    @markers_visited = visited_spots.map do |spot|
       {
         lat: spot.latitude,
         lng: spot.longitude,
-        infoWindow: render_to_string(partial: "maps/info-window", locals: { spot: spot })
+        infoWindow: render_to_string(partial: "maps/info-window", locals: { spot: spot }),
+        image_url: helpers.asset_url('visited.png')
       }
     end
+
+    @markers = @markers_wishlist + @markers_visited
   end
+
+  private
 
   def user_has_coords?
     current_user.latitude.present? && current_user.longitude.present?
