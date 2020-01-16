726d3b5bde0515a950a9006b403fd31c6f56f553
Olivia Goodliffe <ocgoodliffe@gmail.com>
2020-01-17 10:19:11 +1100

    should redirect to newsfeed if user is already logged in

--- app/controllers/katchup_devise_controller.rb
+++ app/controllers/katchup_devise_controller.rb
@@ -1,3 +1,12 @@
 class KatchupDeviseController < ApplicationController
   layout 'without_navbar'
+
+  def new
+    # checks if user is signed in and redirect root to spots_path
+    if user_signed_in?
+      redirect_to newsfeed_path
+    else
+      render layout: 'without_navbar'
+    end
+  end
 end
