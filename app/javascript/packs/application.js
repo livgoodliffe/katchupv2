354a7a6524ce26b9f950144d95d154fe8ba75c2e
Olivia Goodliffe <ocgoodliffe@gmail.com>
2020-02-09 17:28:18 +1100

    orientation portrait

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,11 +10,3 @@ initMapbox();
 searchListener();
 listsAnimations();
 geoLocation();
-
-// if (navigator.serviceWorker) {
-//   navigator.serviceWorker.register('/service-worker.js', { scope: './' })
-//     .then(function(reg) {
-//       console.log('[Companion]', 'Service worker registered!');
-//       console.log(reg);
-//     });
-// }
