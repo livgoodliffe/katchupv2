ddfef0eac1c7faa05211d14530e646e105539e74
Olivia Goodliffe <ocgoodliffe@gmail.com>
2020-02-09 17:24:24 +1100

    header

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,10 @@ searchListener();
 listsAnimations();
 geoLocation();
 
+// if (navigator.serviceWorker) {
+//   navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+//     .then(function(reg) {
+//       console.log('[Companion]', 'Service worker registered!');
+//       console.log(reg);
+//     });
+// }
