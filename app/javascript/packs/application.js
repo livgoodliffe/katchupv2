b2477b03f7e33293ceec134c0e50c4bcc3e02021
Olivia Goodliffe <ocgoodliffe@gmail.com>
2020-02-09 11:48:58 +1100

    service worker load from google

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,25 @@
+// if (navigator.serviceWorker) {
+//   navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+//     .then(function(reg) {
+//       console.log('[Companion]', 'Service worker registered!');
+//       console.log(reg);
+//     });
+// }
+
+// google instructions
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 import "bootstrap";
 import '../navbar';
 import listsAnimations from '../lists';
@@ -6,16 +28,8 @@ import { initMapbox } from '../plugins/init_mapbox';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import geoLocation from '../geolocation';
 
-
 initMapbox();
 searchListener();
 listsAnimations();
 geoLocation();
 
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/service-worker.js', { scope: './' })
-    .then(function(reg) {
-      console.log('[Companion]', 'Service worker registered!');
-      console.log(reg);
-    });
-}
